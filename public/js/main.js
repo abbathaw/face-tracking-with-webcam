85d70adcf05071db4bf89ad5f470c1fa51c2c9df
Abdullah <abdullah.a@servicerocket.com>
2019-01-08 21:57:42 +0800

    Implement face detection by image through webcam

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,5 @@
 let forwardTimes = [];
-let withBoxes = true;
+let withBoxes = false;
 const MODEL_URL = "/models";
 async function loadModels() {
   await faceapi.loadTinyFaceDetectorModel(MODEL_URL);
@@ -35,10 +35,24 @@ async function onPlay() {
   const ts = Date.now();
   const result = await faceapi
     .detectSingleFace(videoEl, options)
-    .withFaceLandmarks();
+    .withFaceLandmarks()
+    .withFaceDescriptor();
   updateTimeStats(Date.now() - ts);
   if (result) {
     drawLandmarks(videoEl, $("#overlay").get(0), [result], withBoxes);
+    const faceDetect = await detectFace();
+    const matchResults = matchUser(faceDetect, result);
+
+    //add label of match
+    const fullFaceDescriptions = [result];
+    const boxesWithText = matchResults.map((bestMatch, i) => {
+      const box = fullFaceDescriptions[i].detection.box;
+      const text = bestMatch.toString();
+      const boxWithText = new faceapi.BoxWithText(box, text);
+      return boxWithText;
+    });
+
+    faceapi.drawDetection($("#overlay").get(0), boxesWithText);
   } else {
     console.log("NO FACE");
   }
@@ -68,4 +82,56 @@ function changeInputSize(size) {
   const inputSizeSelect = $("#inputSize");
   inputSizeSelect.val(inputSize);
   inputSizeSelect.material_select();
+}
+
+// FACE DETECTION
+
+async function detectFace() {
+  const labels = ["abdullah"];
+
+  // tiny_face_detector options
+  let inputSize = 224;
+  let scoreThreshold = 0.5;
+  const options = new faceapi.TinyFaceDetectorOptions({
+    inputSize,
+    scoreThreshold
+  });
+
+  const labeledFaceDescriptors = await Promise.all(
+    labels.map(async label => {
+      // fetch image data from urls and convert blob to HTMLImage element
+      const imgUrl = `${label}.jpg`;
+      const img = await faceapi.fetchImage(imgUrl);
+      console.log("IM HERE1");
+      // detect the face with the highest score in the image and compute it's landmarks and face descriptor
+      const fullFaceDescription = await faceapi
+        .detectSingleFace(img, options)
+        .withFaceLandmarks()
+        .withFaceDescriptor();
+
+      if (!fullFaceDescription) {
+        throw new Error(`no faces detected for ${label}`);
+      }
+
+      const faceDescriptors = [fullFaceDescription.descriptor];
+      console.log("IM HERE");
+      return new faceapi.LabeledFaceDescriptors(label, faceDescriptors);
+    })
+  );
+  return labeledFaceDescriptors;
+}
+
+function matchUser(labeledFaceDescriptors, result) {
+  console.log("what is labeeld face", labeledFaceDescriptors);
+  const maxDescriptorDistance = 0.6;
+  const faceMatcher = new faceapi.FaceMatcher(
+    labeledFaceDescriptors,
+    maxDescriptorDistance
+  );
+  const fullFaceDescriptions = [result];
+  const results = fullFaceDescriptions.map(fd =>
+    faceMatcher.findBestMatch(fd.descriptor)
+  );
+  console.log("Its working");
+  return results;
 }
--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 const viewsDir = path.join(__dirname, "views");
 app.use(express.static(viewsDir));
 app.use(express.static(path.join(__dirname, "./public")));
-app.use(express.static(path.join(__dirname, "../images")));
+app.use(express.static(path.join(__dirname, "./images")));
 app.use(express.static(path.join(__dirname, "../media")));
 
 app.get("/", (req, res) => res.redirect("/index"));
